68ddaa9b28ac963febcdb30415810eb074926375
baemvav1 <baemvav@gmail.com>
2025-11-03 06:18:09 -0600

    Primer commit

---
+++ AdvanceApi/Helpers/DbHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace AdvanceApi.Helpers
+{
+    public class DbHelper
+    {
+
+        private readonly string _connectionString;
+
+        public DbHelper(IConfiguration configuration)
+        {
+            _connectionString = Environment.GetEnvironmentVariable("DefaultConnection") ??
+                                configuration.GetConnectionString("DefaultConnection") ??
+                                throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
