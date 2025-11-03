31957e5400fd4735dbb8c265eaee1aa6a33889fc
baemvav1 <baemvav@gmail.com>
2025-11-03 06:44:11 -0600

    JwtService

---
+++ AdvanceApi/Services/JwtService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AdvanceApi.Services
+{
+    public class JwtService
+    {
+        private readonly string _key;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly int _expireMinutes;
+
+        public JwtService(IConfiguration config)
+        {
+            _key = config["Jwt:Key"]!;
+            _issuer = config["Jwt:Issuer"]!;
+            _audience = config["Jwt:Audience"]!;
+            _expireMinutes = int.Parse(config["Jwt:ExpireMinutes"]!);
+        }
+
+        public string GenerateToken(string username)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_key);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, username),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(_expireMinutes),
+                Issuer = _issuer,
+                Audience = _audience,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
